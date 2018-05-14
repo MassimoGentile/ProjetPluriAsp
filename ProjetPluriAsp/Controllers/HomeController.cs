58d4a3984ccf2e37ca180270112975f8aa55edc6
Matteo GRAVILI <matteo.gravili@condorcet.be>
2018-05-14 19:23:18 +0200

    Ajout des fichiers html et css

---
+++ ProjetPluriAsp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetPluriAsp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.title = "Bienvenue";
+            return View();
+        }
+
+        public ActionResult Inscription()
+        {
+            ViewBag.title = "Inscription";
+            return View();
+        }
+
+        public ActionResult HomePage()
+        {
+            ViewBag.title = "Accueil";
+            return View();
+        }
+
+        public ActionResult Dishes()
+        {
+            ViewBag.title = "Plats";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.title = "Contact";
+            return View();
+        }
+
+
+    }
+}
